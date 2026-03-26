2db2f10ea638ae767a30f97fdbf0d134abbe8801
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2026-03-26 15:39:00 +0530

    flatten (one level) an array using reduce

---
+++ tut-1.js
@@ -0,0 +1,16 @@
+// flate this array using reduce.
+
+let arr = [1, [2,3], [4,5]];
+
+
+// const oneLevelFlat = arr.reduce((acc,curr)=>{
+//     acc = acc.concat(curr);
+//     return acc;
+// },[]);
+
+// console.log(oneLevelFlat);  // [ 1, 2, 3, 4, 5 ]
+
+
+
+// const oneLevelFalte = arr.reduce((acc,curr)=>acc.concat(curr),[]);
+// console.log(oneLevelFalte);  // [ 1, 2, 3, 4, 5 ]
